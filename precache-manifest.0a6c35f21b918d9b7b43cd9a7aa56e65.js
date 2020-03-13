09c4d9b8d3e97abe4fe301e644d6611ae36fe2b2
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-03-13 09:24:30 +0700

    Updates

---
+++ precache-manifest.0a6c35f21b918d9b7b43cd9a7aa56e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d680cb288c8f5a0759d98ccdc1deedf3",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "0821bb6b9717190041e0",
+    "url": "/covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b67ebff492f1c5024c78",
+    "url": "/covid19/static/js/2.19873bba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19/static/js/2.19873bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0821bb6b9717190041e0",
+    "url": "/covid19/static/js/main.b4c5348c.chunk.js"
+  },
+  {
+    "revision": "ca96c5209fe7a20d1994",
+    "url": "/covid19/static/js/runtime-main.9447b676.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19/static/media/logo.5d5d9eef.svg"
+  }
+]);
